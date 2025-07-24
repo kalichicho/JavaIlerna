dad51674f3864e0509501698ef6d7b6984a2de95
kalichicho <romerojuniors15@gmail.com>
2025-07-24 13:04:32 +0200

    arreglo de la sumaTotal

--- matriz/src/code/MatrizAleatoria.java
+++ matriz/src/code/MatrizAleatoria.java
@@ -6,7 +6,7 @@ public class MatrizAleatoria {
 
     // atributos
 
-    int[][] Matriz = new int[2][3];
+    int[][] matriz = new int[2][3];
 
     // objeto Random
     Random random = new Random();
@@ -15,10 +15,10 @@ public class MatrizAleatoria {
 
     public MatrizAleatoria() {
 
-        for (int i = 0; i < Matriz.length; i++) { // recorremos las filas de la matriz
+        for (int i = 0; i < matriz.length; i++) { // recorremos las filas de la matriz
 
-            for (int j = 0; j < Matriz[i].length; j++) { // recorremos las columnas de la matriz
-                Matriz[i][j] = random.nextInt(3);
+            for (int j = 0; j < matriz[i].length; j++) { // recorremos las columnas de la matriz
+                matriz[i][j] = random.nextInt(3);
             }
         }
 
@@ -27,22 +27,22 @@ public class MatrizAleatoria {
     // getters and setter
 
     public int[][] getMatriz() {
-        return Matriz;
+        return matriz;
     }
 
     public void setMatriz(int[][] matriz) {
-        this.Matriz = matriz;
+        this.matriz = matriz;
     }
 
     // metodos
 
     public void imprimirMatriz() {
 
-        for (int i = 0; i < Matriz.length; i++) { // recorremos las filas de la matriz
+        for (int i = 0; i < matriz.length; i++) { // recorremos las filas de la matriz
 
-            for (int j = 0; j < Matriz[i].length; j++) { // recorremos las columnas de la matriz
+            for (int j = 0; j < matriz[i].length; j++) { // recorremos las columnas de la matriz
 
-                System.out.print(Matriz[i][j] + " ");
+                System.out.print(matriz[i][j] + " ");
 
             }
 
@@ -51,36 +51,37 @@ public class MatrizAleatoria {
 
     }
 
+    // metodo para imprimir la matriz más la suma de sus valores
     public void imprimirSumaMatriz() {
 
-        int[] sumaFila = new int[Matriz.length];
-        int[] sumaColumna = new int[Matriz[0].length];
+        int[] sumaFila = new int[matriz.length];
+        int[] sumaColumna = new int[matriz[0].length];
         int sumaTotal = 0;
 
         // bucle para sumar las filas
 
-        for (int i = 0; i < Matriz.length; i++) { // recorremos las filas de la matriz
+        for (int i = 0; i < matriz.length; i++) { // recorremos las filas de la matriz
             sumaFila[i] = 0;
-            for (int j = 0; j < Matriz[i].length; j++) { // recorremos las columnas de la matriz
+            for (int j = 0; j < matriz[i].length; j++) { // recorremos las columnas de la matriz
 
-                sumaFila[i] = sumaFila[i] + Matriz[i][j];
+                sumaFila[i] = sumaFila[i] + matriz[i][j];
 
-                System.out.print(Matriz[i][j] + " ");
+                System.out.print(matriz[i][j] + " ");
 
             }
 
-            System.out.print(sumaFila[i]);
+            System.out.print("|" + Colores.VERDE + sumaFila[i] + Colores.RESET);
 
             System.out.println();
         }
 
         // bucle para sumar las columnas
 
-        for (int j = 0; j < Matriz[0].length; j++) { // recorremos las columnas
+        for (int j = 0; j < matriz[0].length; j++) { // recorremos las columnas
 
-            for (int i = 0; i < Matriz.length; i++) {
+            for (int i = 0; i < matriz.length; i++) {
 
-                sumaColumna[j] = sumaColumna[j] + Matriz[i][j];
+                sumaColumna[j] = sumaColumna[j] + matriz[i][j];
 
             }
 
@@ -88,26 +89,21 @@ public class MatrizAleatoria {
 
         }
 
-        //sumamos todos los valores del array que contiene la suma de las filas
+        // sumamos todos los valores del array que contiene la suma de las filas
 
-        for(int i = 0; i <sumaFila.length; i++){
+        for (int i = 0; i < sumaFila.length; i++) {
 
-            sumaTotal = sumaTotal+ sumaFila[i];
-
-        }
-
-        //sumamos todos los valores del array que contiene las sumas de las columnas
-
-        for(int i = 0 ; i <sumaColumna.length; i++){
-
-            sumaTotal = sumaTotal + sumaColumna[i];
+            sumaTotal = sumaTotal + sumaFila[i];
 
         }
 
-        //imprimimos el último valor del gráfico que representa la suma de todas las columnas y de todas las filas
+        // imprimimos el último valor del gráfico que representa la suma de todas las
+        // columnas y de todas las filas
 
-        System.out.print(sumaTotal);
-        
+       
+
+        System.out.print("|" + Colores.ROJO + sumaTotal + Colores.RESET);
+        System.out.println();
 
     }
 
