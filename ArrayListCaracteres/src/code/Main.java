8753302d6dd8f8f168684500ee6ac06a9cc579f3
David Emanuel Romero <92949594+kalichicho@users.noreply.github.com>
2025-07-27 14:13:33 +0200

    añadimos ejercicio

---
+++ ArrayListCaracteres/src/code/Main.java
@@ -0,0 +1,72 @@
+package code;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Character> lista = new ArrayList<>();
+
+        lista.add('c');
+        lista.add('d');
+        lista.add('f');
+
+        // Bucle para mostrar datos
+        for (int i = 0; i < lista.size(); i++) {
+
+            System.out.print(lista.get(i));
+        }
+        System.out.println();
+
+        // añadimos datos
+        lista.add(0, 'a');
+        lista.add(0, 'b');
+
+        // Bucle para mostrar datos
+        for (int i = 0; i < lista.size(); i++) {
+
+            System.out.print(lista.get(i));
+        }
+
+        System.out.println();
+        // añadimos datos
+        lista.add('g');
+
+        // Bucle para mostrar datos
+        for (int i = 0; i < lista.size(); i++) {
+
+            System.out.print(lista.get(i));
+        }
+
+        System.out.println();
+        // bucle para agregar un caracter entre 2
+        for (int i = 0; i < lista.size(); i++) {
+
+            if (lista.get(i) == 'f') {
+
+                lista.add(i, 'e'); // añadimos datos
+                break;
+
+            }
+
+        }
+
+        // Bucle para mostrar datos
+        for (int i = 0; i < lista.size(); i++) {
+
+            System.out.print(lista.get(i));
+        }
+
+        System.out.println();
+        // eliminamos un caracter por indice
+        lista.remove(2);
+
+        // Bucle para mostrar datos
+        for (int i = 0; i < lista.size(); i++) {
+
+            System.out.print(lista.get(i));
+        }
+        System.out.println();
+    }
+
+}
